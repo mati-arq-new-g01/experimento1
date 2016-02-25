335859da8f1070a11e794b0fca632bc78aa570e0
mati-arq-new-g01 <oi.estrada@uniandes.edu.co>
2016-02-24 23:58:12 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,6 +10,3 @@ apt-get update
 # Instala Docker
 curl -sSL https://get.docker.com/ | sh
 ##
-docker run -p 3000:3000 --name microservices -v /vagrant/repositorio:/usr/local/ -h microservices -itd oiestradag/nodejs-seneca
-
-docker run -p 80:3000 --name apigateway -h apigateway -itd oiestradag/nodejs-apigateway
